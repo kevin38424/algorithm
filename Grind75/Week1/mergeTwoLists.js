72e1f712f521fc9c5b0083bcb080d416ff948253
Kevin Park-Lee <kevin38424@gmail.com>
2022-07-31 01:11:04 -0400

    mergeTwoLists

--- Grind75/Week1/mergeTwoLists.js
+++ Grind75/Week1/mergeTwoLists.js
@@ -20,18 +20,38 @@
 // Input: list1 = [], list2 = [0]
 // Output: [0]
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+
+  function ListNode(val, next) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+  }
 /**
  * @param {ListNode} list1
  * @param {ListNode} list2
  * @return {ListNode}
  */
  var mergeTwoLists = function(list1, list2) {
-
+    const head = new ListNode();
+    let curNode = head;
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            curNode.next = list1;
+            list1 = list1.next;
+        } else {
+            curNode.next = list2;
+            list2 = list2.next;
+        }
+        curNode = curNode.next;
+    }
+    curNode.next = list1 || list2;
+    return head.next;
 };
+
+let l1 = new ListNode(1);
+l1.next = new ListNode(2);
+l1.next.next = new ListNode(4);
+let l2 = new ListNode(1);
+l2.next = new ListNode(3);
+l2.next.next = new ListNode(4);
+
+console.log(mergeTwoLists(l1,l2))
