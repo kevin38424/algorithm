30b570e3d6319eb6ddf686b1bc3e80a5c6e1a511
Kevin Park-Lee <kevin38424@gmail.com>
2022-08-04 22:25:45 -0400

    lengthOfLongestSubstring

---
+++ Amazon/Arrays_Strings/lengthOfLongestSubstring.js
@@ -0,0 +1,38 @@
+// Given a string s, find the length of the longest substring without repeating characters.
+
+// Example 1:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Example 2:
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+// Example 3:
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+// Constraints:
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
+function lengthOfLongestSubstring(str) {
+  const map = new Map();
+  let start = 0;
+  let maxLength = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (map.has(str[i])) start = Math.max(map.get(str[i]) + 1, start);
+    map.set(str[i], i);
+    maxLength = Math.max(maxLength, i - start + 1);
+    console.log(map);
+    console.log(start);
+  }
+
+  return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("abba"));
