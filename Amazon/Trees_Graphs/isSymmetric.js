b46155ec9e2c8188df99f70b473c5d241dc5fec5
Kevin Park-Lee <kevin38424@gmail.com>
2022-08-09 11:01:31 -0400

    08092022

---
+++ Amazon/Trees_Graphs/isSymmetric.js
@@ -0,0 +1,44 @@
+// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+// Example 1:
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true
+
+// Example 2:
+// Input: root = [1,2,2,null,3,null,3]
+// Output: false
+
+// Constraints:
+// The number of nodes in the tree is in the range [1, 1000].
+// -100 <= Node.val <= 100
+
+// Follow up: Could you solve it both recursively and iteratively?
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  function isMirror(leftNode, rightNode) {
+    if (leftNode === null && rightNode === null) return true;
+    if (
+      leftNode === null ||
+      rightNode === null ||
+      leftNode.val !== rightNode.val
+    )
+      return false;
+    return (
+      isMirror(leftNode.left, rightNode.right) &&
+      isMirror(leftNode.right, rightNode.left)
+    );
+  }
+  return isMirror(root, root);
+};
