4431cc40e1678c6531e6582a9fd32b93aefc2395
Kevin Park-Lee <kevin38424@gmail.com>
2022-08-19 11:09:07 -0400

    08192022

---
+++ Practice/canSchedule.js
@@ -0,0 +1,10 @@
+function canSchedule(meetings, start, end) {}
+
+const meetings = [
+  [1300, 1500],
+  [930, 1200],
+  [830, 845],
+];
+const start = 820;
+const end = 830;
+//this should return true;
